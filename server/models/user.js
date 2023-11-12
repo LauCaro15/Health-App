7ce8e2ed46fdea3f9d9adab196d3689303b7f86e
CamiloM <juanc.munozv@autonoma.edu.co>
2023-11-11 22:50:36 -0500

    Create basic database with mongoDB

---
+++ server/models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    name: { type: String, require: true  },
+    lastname: { type: String, require: true },
+    email: { type: String, require: true, unique: true },
+    password: { type: String, require: true},
+    active: {  type: Boolean, require: true, default: false},
+    rol: { type: String, require: true },
+});
+
+module.exports = mongoose.model("User", userSchema);
